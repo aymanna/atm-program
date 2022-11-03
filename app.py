f7ab0057355a0be7da0e50602d36bd265944f895
Ayman Na <natanagara.ayman@gmail.com>
2022-11-04 00:38:03 +0700

    move main function

--- app.py
+++ app.py
@@ -1,3 +1,100 @@
-import atm
+import dbMethod
+from atm import ATM
 
-atm.main()
+
+def main():
+
+    print("Selamat datang di ATM A. Silahkan masukkan ID Anda.\n")
+
+    id_input = input("input ID: ")
+    while not dbMethod.in_data(id_input):
+        print("ID yang Anda masukkan tidak berlaku. Silahkan coba kembali.\n")
+        id_input = input("input ID: ")
+
+    pin_input = input("input PIN: ")
+    while not dbMethod.true_user(id_input, pin_input):
+        print("PIN yang Anda masukkan salah. Silahkan coba kembali.\n")
+        pin_input = input("input PIN: ")
+
+    user = ATM(id_input)
+
+    menus = [
+        "Cek Saldo",
+        "Setor Tunai",
+        "Tarik Tunai",
+        "Transfer",
+        "Riwayat Transaksi",
+        "Keluar",
+    ]
+
+    print(f"\nSelamat datang, {user.name}.")
+
+    while True:
+
+        print("\nSilahkan pilih menu di bawah ini.")
+        for i, menu in enumerate(menus):
+            print("[%d] %s" % (i + 1, menu))
+        print()
+
+        menu_input = input("Pilihan menu: ")
+
+        if menu_input == '1':
+            print(user.formatted_balance)
+            continue
+
+        elif menu_input == '2':
+            try:
+                amount = int(input("\nMasukkan jumlah setoran: "))
+            except:
+                print("Setoran tunai gagal.")
+            else:
+                if amount <= 0:
+                    print("Setoran tunai gagal.")
+                    continue
+                user.deposit(amount)
+            continue
+
+        elif menu_input == '3':
+            try:
+                amount = int(input("\nMasukkan jumlah penarikan: "))
+            except:
+                print("Penarikan tunai gagal.")
+            else:
+                if amount <= 0:
+                    print("Penarikan tunai gagal.")
+                    continue
+                user.withdraw(amount)
+            continue
+
+        elif menu_input == '4':
+            transfer_id = input("\nMasukkan ID yang ingin ditransfer: ")
+
+            try:
+                amount = int(input("Masukkan jumlah transfer: "))
+            except:
+                print("Transfer gagal.")
+            else:
+                if amount <= 0:
+                    print("Transfer gagal.")
+                    continue
+                print()
+                user.transfer(transfer_id, amount)
+            continue
+
+        elif menu_input == '5':
+            print()
+            user.transaction_history()
+            continue
+
+        elif menu_input == '6':
+            print("\nTerima kasih telah menggunakan ATM A, %s." % user.name)
+            print("Selamat Tinggal!")
+            break
+
+        else:
+            print()
+            continue
+
+
+if __name__ == '__main__':
+    main()
--- atm.py
+++ atm.py
@@ -70,130 +70,32 @@ class ATM(Customer):
             return
 
         columns = ["Jumlah", "Deskripsi", "Tanggal"]
-        mxLengths = []
+        max_col_lengths = []
         sep = "+"
 
         for i in range(len(columns)):
-            transaction = max(transactions, key=lambda x: x[i])
-            dataLength = len(str(transaction[i]))
+            mx_transaction = max(transactions, key=lambda x: x[i])
+            data_length = len(str(mx_transaction[i]))
 
-            if dataLength > len(columns[i]):
-                maxLength = dataLength
+            if data_length > len(columns[i]):
+                max_col_length = data_length
             else:
-                maxLength = len(columns[i])
+                max_col_length = len(columns[i])
 
-            mxLengths.append(maxLength)
-            sep += "-" * (maxLength + 2) + "+"
+            max_col_lengths.append(max_col_length)
+            sep += "-" * (max_col_length + 2) + "+"
 
         print(sep)
 
         for i, column in enumerate(columns):
-            print(f"| {column.ljust(mxLengths[i])} ", end='')
+            print(f"| {column.ljust(max_col_lengths[i])} ", end='')
 
         print('|')
         print(sep)
 
-        for transaction in transactions:
-            for i, item in enumerate(transaction):
-                print(f"| {str(item).ljust(mxLengths[i])} ", end='')
+        for mx_transaction in transactions:
+            for i, item in enumerate(mx_transaction):
+                print(f"| {str(item).ljust(max_col_lengths[i])} ", end='')
             print('|')
 
         print(sep)
-
-
-def main():
-
-    print("Selamat datang di ATM A. Silahkan masukkan ID Anda.\n")
-
-    id_input = input("input ID: ")
-    while not dbMethod.in_data(id_input):
-        print("ID yang Anda masukkan tidak berlaku. Silahkan coba kembali.\n")
-        id_input = input("input ID: ")
-
-    pin_input = input("input PIN: ")
-    while not dbMethod.true_user(id_input, pin_input):
-        print("PIN yang Anda masukkan salah. Silahkan coba kembali.\n")
-        pin_input = input("input PIN: ")
-
-    user = ATM(id_input)
-
-    menus = [
-        "Cek Saldo",
-        "Setor Tunai",
-        "Tarik Tunai",
-        "Transfer",
-        "Riwayat Transaksi",
-        "Keluar",
-    ]
-
-    print("\nSelamat datang, %s." % user.name)
-
-    while True:
-
-        print("\nSilahkan pilih menu di bawah ini.")
-        for i, menu in enumerate(menus):
-            print("[%d] %s" % (i + 1, menu))
-        print()
-
-        menu_input = input("Pilihan menu: ")
-
-        if menu_input == '1':
-            print(user.formatted_balance)
-            continue
-
-        elif menu_input == '2':
-            try:
-                amount = int(input("\nMasukkan jumlah setoran: "))
-            except:
-                print("Setoran tunai gagal.")
-            else:
-                if amount <= 0:
-                    print("Setoran tunai gagal.")
-                    continue
-                user.deposit(amount)
-            continue
-
-        elif menu_input == '3':
-            try:
-                amount = int(input("\nMasukkan jumlah penarikan: "))
-            except:
-                print("Penarikan tunai gagal.")
-            else:
-                if amount <= 0:
-                    print("Penarikan tunai gagal.")
-                    continue
-                user.withdraw(amount)
-            continue
-
-        elif menu_input == '4':
-            transfer_id = input("\nMasukkan ID yang ingin ditransfer: ")
-
-            try:
-                amount = int(input("Masukkan jumlah transfer: "))
-            except:
-                print("Transfer gagal.")
-            else:
-                if amount <= 0:
-                    print("Transfer gagal.")
-                    continue
-                print()
-                user.transfer(transfer_id, amount)
-            continue
-
-        elif menu_input == '5':
-            print()
-            user.transaction_history()
-            continue
-
-        elif menu_input == '6':
-            print("\nTerima kasih telah menggunakan ATM A, %s." % user.name)
-            print("Selamat Tinggal!")
-            break
-
-        else:
-            print()
-            continue
-
-
-if __name__ == '__main__':
-    main()
